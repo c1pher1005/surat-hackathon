f2e04c89134107fd917cd0c2391f898bf03926ce
Abhijeet Singh <106686793+abhijeetsinghCXT@users.noreply.github.com>
2023-11-04 14:09:42 +0530

    Update dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -19,34 +19,11 @@ export default function Dashboard() {
   useEffect(() => {
     if (!isConnected && logoutFlag) {
       sessionStorage.clear()
-      window.location.href = "/"
+      window.location.href = "https://mythvendix.000webhostapp.com/surat/judge/template/"
     }
   }, [isConnected])
 
   function logout() {
     setLogoutFlag(true)
   }
-
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="absolute bottom-10 right-10">
-        <button onClick={() => setIsChatbotOpen(!isChatbotOpen)}>
-          <img src={chatbot} alt="chatbot" width={69} height={69} />
-        </button>
-      </div>
-      {
-        isChatbotOpen && <div className="absolute right-5 top-5 w-[92vw] sm:w-[50vw] h-[80vh] z-30 bg-black/50">
-          <iframe src="https://lawbotpro.com/" className="w-[92vw] sm:w-[50vw] h-[80vh]" title="chatbot" />
-        </div>
-      }
-      <div className="w-full min-h-screen">
-        <div className="w-full p-3 font-bold text-xl bg-amber-300 flex justify-between items-center">
-          <div>{userType} DASHBOARD</div>
-          <div onClick={logout} className="bg-white rounded-full">
-            <w3m-button />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
 }
